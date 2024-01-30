5ac2ea747e987ff4059b29d29ff003750153239f
alex <Bodorazor@gmail.com>
2024-01-29 18:24:38 -0600

    mi proyecto de vida

---
+++ Desktop/ProyectosAlex/maquedaxd/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8214372ffc9841423688f0656624bb2c'),
+    },
+  },
+});
---
+++ Desktop/ProyectosAlex/maquedaxd/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bd6391d2-7174-413b-9ad3-f8a2517d2cbe'
+};
